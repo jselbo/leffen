216b9060e65bc04e0044a7a639ca0c89fc4e93f2
hanvo <donphancong12@gmail.com>
2017-02-04 10:20:29 -0600

    Bug(WhiteSpace): #Trigger Josh

--- messenger/main.js
+++ messenger/main.js
@@ -10,11 +10,11 @@ app.get('/', (req, res) => {
 });
 
 app.post('/', jsonParser, (req, res) => {
-    const url = req.body.URL;
-    const videoId = req.body.VideoID;
-    const title = req.body.Title;
-    discordBot.sendMessage(url, videoId, title);
-    res.send('Discord Bot Message Sent!');
+  const url = req.body.URL;
+  const videoId = req.body.VideoID;
+  const title = req.body.Title;
+  discordBot.sendMessage(url, videoId, title);
+  res.send('Discord Bot Message Sent!');
 });
 
 app.listen(3000, () =>  {
